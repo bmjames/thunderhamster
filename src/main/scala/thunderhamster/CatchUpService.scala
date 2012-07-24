5dae12ca709e3efd855f450aab3fb8f8294dba7d
Ben James <ben.james@guardian.co.uk>
2012-07-24 22:27:32 +0100

    Add /quick and /long endpoints

--- src/main/scala/thunderhamster/CatchUpService.scala
+++ src/main/scala/thunderhamster/CatchUpService.scala
@@ -6,8 +6,12 @@ import blueeyes.core.data.{BijectionsChunkJson, BijectionsChunkString}
 import blueeyes.core.service.engines.HttpClientXLightWeb
 import blueeyes.core.http.MimeTypes.{json, application}
 import blueeyes.core.data.ByteChunk
-import blueeyes.core.http.{HttpResponse, HttpRequest}
+import blueeyes.core.http._
 import blueeyes.json.JsonAST.JValue
+import blueeyes.core.http.HttpStatusCodes.Found
+import blueeyes.core.http.HttpHeaders.Location
+import scalaz._, Scalaz._
+import ApplicativeInstances._, MonoidInstances._
 
 
 trait CatchUpService extends BlueEyesServiceBuilder
@@ -24,13 +28,26 @@ trait CatchUpService extends BlueEyesServiceBuilder
       produce(application/json) {
 
         path("/") {
-          get { request: HttpRequest[ByteChunk] =>
+          get { _: HttpRequest[ByteChunk] =>
+            FoundResponse(location="/quick")
+          }
+        } ~
+        path("/quick") {
+          get { _: HttpRequest[ByteChunk] =>
 
             val shortStories = api.getArticles(limit=3, maxWordcount=500, minWordcount=50)
             val medStory = api.getArticles(limit=1, maxWordcount=1000, minWordcount=501)
             val longStory = api.getArticles(limit=1, maxWordcount=2000, minWordcount=1001)
 
             BundleResponse(shortStories, medStory, longStory)
+          }
+        } ~
+        path("/long") {
+          get { _: HttpRequest[ByteChunk] =>
+
+            val longStories = api.getArticles(limit=3, maxWordcount=20000, minWordcount=2000)
+
+            BundleResponse(longStories)
           }
         }
 
@@ -42,8 +59,11 @@ trait CatchUpService extends BlueEyesServiceBuilder
 }
 
 object BundleResponse {
-  import scalaz._, Scalaz._
-  import ApplicativeInstances._, MonoidInstances._
   def apply(responses: Future[Option[JValue]]*)(implicit ex: ExecutionContext): Future[HttpResponse[JValue]] =
     responses.toList.sequence map (stories => HttpResponse(content = stories.suml))
+}
+
+object FoundResponse {
+  def apply[T](location: T)(implicit ex: ExecutionContext, ev: T => URI): Future[HttpResponse[JValue]] =
+    Future(HttpResponse[JValue](status=HttpStatus(Found), headers=HttpHeaders(Seq(Location(location)))))
 }
